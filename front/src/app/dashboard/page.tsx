97f5afa3954cab0888490f9a82190d3e2fa2b7ec
Raul Belloso Medina <raul.belloso.m@gmail.com>
2025-02-13 13:31:20 -0600

    add Tailwind CSS configuration and utility components for styling

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,15 +6,15 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar";
+} from "@/components/ui/sidebar"
 
-const Dashboard = () => {
+export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -40,15 +40,13 @@ const Dashboard = () => {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
+            <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
+            <div className="aspect-video rounded-xl bg-neutral-100/50 dark:bg-neutral-800/50" />
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-neutral-100/50 md:min-h-min dark:bg-neutral-800/50" />
         </div>
       </SidebarInset>
     </SidebarProvider>
-  );
-};
-
-export default Dashboard;
+  )
+}
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/auth";
 import { useNavigate } from "react-router-dom";
+import { LoginForm } from "@/components/login-form";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -16,28 +17,10 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 shadow-md rounded-md"
-      >
-        <h2 className="text-xl font-bold mb-4">Iniciar Sesión</h2>
-        <input
-          type="email"
-          placeholder="Correo"
-          className="border p-2 w-full mb-2"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className="border p-2 w-full mb-4"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="bg-blue-500 text-white py-2 px-4 rounded-md w-full">
-          Ingresar
-        </button>
-      </form>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
     </div>
   );
 };
